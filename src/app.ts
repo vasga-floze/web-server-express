8ccba08399d2b99f1b27febae42402ecaf61d888
Gabriela Flores <gabriela.floze@gmail.com>
2024-04-12 14:17:40 -0600

    first server using http/1

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import http from 'http';
 
+const server = http.createServer((req, res) => {
 
-console.log("Hola Mundo");
+    console.log(req.url);
+    res.write('Hola Mundo');
+    res.end();
+
+});
+
+server.listen(8080, () => {
+    console.log('Server running on port 8080');
+})
